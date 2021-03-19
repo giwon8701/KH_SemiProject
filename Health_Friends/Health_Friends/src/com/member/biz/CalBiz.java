08d6f99ca41783ce60ff68c977a0ec0d08735a9b
강원기(KangWonKi) <78646944+KangCine@users.noreply.github.com>
2021-03-19 14:17:04 +0900

    수정사항

---
+++ Health_Friends/Health_Friends/src/com/member/biz/CalBiz.java
@@ -0,0 +1,17 @@
+package com.member.biz;
+
+import java.util.List;
+
+import com.member.dto.CalDto;
+
+public interface CalBiz {
+	
+	int insertCalBoard(CalDto dto);
+	public List<CalDto> getCalList(String id, String yyyyMMdd);
+	public List<CalDto> getCalViewList(String id, String yyyyMM);
+	public int getCalViewCount(String id, String yyyyMMdd);
+	public CalDto selectOneCalBoard(int seq);
+	public int updateCalBoard(CalDto dto);
+	public int deleteCalBoard(int seq);
+	
+}
